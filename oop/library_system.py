9b67ec62d508acd98e348d7a594eea1e0b6332d3
Khadija Bekkali <khadijabekkali44@gmail.com>
2025-12-07 20:05:13 +0100

    final edit

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,13 +3,35 @@ class Book:
         self.title = title
         self.author = author
 
-    # Required by checker
     def get_info(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def get_info(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    # Required by checker
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
     # Used by your main.py
     def details(self):
-        return self.get_info()
+        return str(self)
 
 
 class EBook(Book):
@@ -18,14 +40,12 @@ class EBook(Book):
         self.file_size = file_size
 
     # Required by checker
-    def get_info(self):
-        return (
-            f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
-        )
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
     # Used by your main.py
     def details(self):
-        return self.get_info()
+        return str(self)
 
 
 class PrintBook(Book):
@@ -34,14 +54,12 @@ class PrintBook(Book):
         self.page_count = page_count
 
     # Required by checker
-    def get_info(self):
-        return (
-            f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-        )
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
     # Used by your main.py
     def details(self):
-        return self.get_info()
+        return str(self)
 
 
 class Library:
@@ -51,7 +69,22 @@ class Library:
     def add_book(self, book):
         self.books.append(book)
 
-    # The checker expects get_info()
+    # Use __str__() for printing
+    def list_books(self):
+        for book in self.books:
+            print(book)
+
+    def get_info(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
     def list_books(self):
         for book in self.books:
             print(book.get_info())
